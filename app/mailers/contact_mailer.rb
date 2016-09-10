881574ce8604a333b5b937323ef7af5fbd862600
Foliwe <foliwe@gmail.com>
2016-09-11 00:53:29 +0100

    Devise updated

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,7 +1,7 @@
 class ContactMailer < ActionMailer::Base
 
   default from: "Your Mailer <noreply@yourdomain.com>"
-  default to: "Your Name <foliwe@gmail.com>"
+  default to: "Your Name <drnjualem@gmail.com>"
 
   def new_contact(contact)
     @contact = contact
