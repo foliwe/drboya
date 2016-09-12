3386bdbc1210d0d3cc484c644882896b419534a4
Foliwe <foliwe@gmail.com>
2016-09-12 23:19:16 +0100

    update refile

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -4,7 +4,7 @@ require "refile/s3"
 aws = {
   access_key_id: Rails.application.secrets.access_key,
   secret_access_key: Rails.application.secrets.secret_key,
-  region: Rails.application.secrets.region,
+  region:  "eu-west-1",
   bucket: Rails.application.secrets.bucket,
 }
 Refile.cache = Refile::S3.new(prefix: "cache", **aws)
